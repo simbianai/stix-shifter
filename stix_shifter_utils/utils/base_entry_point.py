6e3df00dfd5f7d288a1024e479ac9a7e229e74cc
Prateek Sinha <prateeksinha18@gmail.com>
2025-01-05 22:59:33 +0530

    fix

--- stix_shifter_utils/utils/base_entry_point.py
+++ stix_shifter_utils/utils/base_entry_point.py
@@ -167,17 +167,14 @@ class BaseEntryPoint:
         """
         default_results_translator:BaseResultTranslator = self.__dialect_to_results_translator[self.__dialect_default[self.__options.get(OPTION_LANGUAGE, "stix")]]
         results_translator = copy.deepcopy(default_results_translator)
-        print("results_translator map_data default: ", results_translator.map_data)
         for dialect, translator in self.__dialect_to_results_translator.items():
             if dialects and dialect not in dialects:
                 continue
-            results_translator.map_data = deep_merge_dicts(results_translator.map_data, translator.map_data)
-        print("results_translator map_data: ", results_translator.map_data)
+            results_translator.map_data = {**results_translator.map_data, **translator.map_data}
         return results_translator
 
     @translation
     def get_results_translator(self, dialect:Union[str,list]=None):
-        print("dialect: ", dialect)
         if dialect and isinstance(dialect, str):
             return self.__dialect_to_results_translator[dialect]
         return self.__combine_default_results_translator_to_stix_maps(dialect)
@@ -362,13 +359,3 @@ class BaseEntryPoint:
 
     def is_async(self):
         return self.__async
-    
-def deep_merge_dicts(base: dict, new: dict):
-        for key, value in new.items():
-            if (key in base 
-                and isinstance(base[key], dict) 
-                and isinstance(value, dict)):
-                deep_merge_dicts(base[key], value)
-            else:
-                base[key] = value
-        return base
